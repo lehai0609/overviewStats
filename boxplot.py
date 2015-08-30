77649d5b8a6fabaaa458920f775c9c63e5f99e6b
Le Quang Hai <lehai0609@gmail.com>
2015-08-30 11:58:35 +0700

    Plotting

---
+++ boxplot.py
@@ -0,0 +1,19 @@
+import numpy as np
+import scipy.stats as stats
+import matplotlib.pyplot as plt
+
+test_data = np.random.normal(size=1000)
+
+###################Boxplot################
+plt.boxplot(test_data)
+plt.show()
+
+###################Histogram##############
+plt.hist(test_data, histtype='bar')
+plt.show()
+
+###################QQ-plot###############
+plt.figure()
+qqPlot = stats.probplot(test_data,dist='norm', plot=plt)
+plt.show()
+
